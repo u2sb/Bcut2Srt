2dab99f6ca37f002c8cefdd1f20b9ff155517eda
MonoLogueChi <xxwhite.com@gmail.com>
2022-06-10 17:47:58 +0800

    添加使用必剪上传视频功能

---
+++ Bcut2Srt/Models/WorksInfos.cs
@@ -0,0 +1,23 @@
+﻿namespace Bcut2Srt.Models;
+
+internal class WorksInfos
+{
+    public WorksInfos()
+    {
+        worksInfos = new List<WorksInfo>();
+    }
+
+    public List<WorksInfo> worksInfos { get; set; }
+}
+
+public class WorksInfo
+{
+    public string draftId { get; set; }
+    public int duration { get; set; }
+    public string? filePath { get; set; }
+    public string id { get; set; }
+    public float imageRatio { get; set; }
+    public long modifyTime { get; set; }
+    public string name { get; set; }
+    public int status { get; set; }
+}
--- Bcut2Srt/Program.cs
+++ Bcut2Srt/Program.cs
@@ -1,27 +1,100 @@
-﻿using System.Text.Json;
+﻿using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
 using Bcut2Srt.Models;
+using SubtitleManager;
+
+namespace Bcut2Srt;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        if (args.Length == 1)
+        if (args.Length > 0 && !string.IsNullOrWhiteSpace(args[0]))
         {
-            using var bs = new StreamReader(args[0]);
-            var options = new JsonSerializerOptions
+            var path = args[0];
+            var jsonSerializerOptions = new JsonSerializerOptions
             {
-                PropertyNameCaseInsensitive = true
+                PropertyNameCaseInsensitive = true,
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true,
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.All)
             };
-            var bj = JsonSerializer.Deserialize<BcutProjectJson>(bs.ReadToEnd(), options);
-            var bja = bj.tracks.SelectMany(a => a.clips)
-                .Where(b => b._30021 > 0 && !string.IsNullOrWhiteSpace(b.AssetInfo.content))
-                .Select(c => ToSrtSingle(c._30021, c._30012, c.AssetInfo.content)).ToArray();
-            var bjs = ToSrt(bja);
 
-            File.WriteAllText("./导出字幕.srt", bjs);
+            // json格式 工程草稿文件 用于导出字幕
+            if (path.ToLower().EndsWith(".json"))
+            {
+                using var bs = new StreamReader(path);
+
+                var bj = JsonSerializer.Deserialize<BcutProjectJson>(bs.ReadToEnd(), jsonSerializerOptions);
+
+                var number = 0;
+
+                var bja = bj?.tracks.SelectMany(a => a.clips)
+                    .Where(b => b._30021 > 0 && !string.IsNullOrWhiteSpace(b.AssetInfo.content))
+                    .Select(c => new SrtSubtitleLine
+                    {
+                        LineNumber = ++number,
+                        Start = new TimeSpan(0, 0, 0, 0, c._30021),
+                        End = new TimeSpan(0, 0, 0, 0, c._30012 + c._30021),
+                        Text = c.AssetInfo.content
+                    } as ISubtitleLine).ToList();
+                var bjs = new SrtSubtitle
+                {
+                    Lines = bja
+                };
+
+                File.WriteAllText("./导出字幕.srt", bjs.ToString());
 
 
-            Console.WriteLine("转换完成");
+                Console.WriteLine("转换完成");
+            }
+
+            // 视频格式，用于添加工程
+            else if (path.ToLower().EndsWith(".mp4") ||
+                     path.ToLower().EndsWith(".mov") ||
+                     path.ToLower().EndsWith(".flv"))
+            {
+                var worksInfoPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+                    "Bcut Drafts", "worksInfo.json");
+
+
+                using (var fs = File.Open(worksInfoPath, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+                {
+                    var sr = new StreamReader(fs);
+                    var json = sr.ReadToEnd();
+                    var wis = !string.IsNullOrWhiteSpace(json)
+                        ? JsonSerializer.Deserialize<WorksInfos>(json, jsonSerializerOptions)
+                        : new WorksInfos();
+                    var wi = new WorksInfo
+                    {
+                        draftId = Guid.NewGuid().ToString().ToUpper(),
+                        duration = 1,
+                        filePath = path,
+                        id = Guid.NewGuid().ToString().ToUpper(),
+                        imageRatio = 1,
+                        modifyTime = (long)(DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0)).TotalMilliseconds,
+                        name = Path.GetFileNameWithoutExtension(path),
+                        status = 0
+                    };
+
+                    wis ??= new WorksInfos();
+                    wis.worksInfos.Add(wi);
+
+                    var w = JsonSerializer.SerializeToUtf8Bytes(wis, jsonSerializerOptions);
+
+                    fs.Seek(0, SeekOrigin.Begin);
+                    fs.SetLength(0);
+
+                    fs.Write(w, 0, w.Length);
+
+                    fs.Close();
+
+                    Console.WriteLine("写入完成，请重新打开必剪");
+                }
+            }
+
+            Console.WriteLine("按任意键退出");
         }
 
         else
@@ -29,28 +102,7 @@ internal class Program
             Console.WriteLine("请指定输入文件");
         }
 
+
         Console.ReadKey();
-    }
-
-    private static string ToSrtSingle(int start, int l, string content)
-    {
-        var startTimeStr = new DateTime(0).AddMilliseconds(start).ToString("HH:mm:ss,fff");
-        var endTimeStr = new DateTime(0).AddMilliseconds(start).AddMilliseconds(l).ToString("HH:mm:ss,fff");
-
-        return $"{startTimeStr}-->{endTimeStr}\r\n{content}\r\n";
-    }
-
-    private static string ToSrt(string[] srtSingle)
-    {
-        var a = "";
-        for (var i = 0; i < srtSingle.Length; i++)
-        {
-            a += i + 1;
-            a += "\r\n";
-            a += srtSingle[i];
-            a += "\r\n";
-        }
-
-        return a;
     }
 }
